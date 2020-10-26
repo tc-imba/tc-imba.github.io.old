8875f487c6cfe241313795c5b114abb23c658186
tc-imba <liuyh615@sjtu.edu.cn>
2020-10-27 04:04:12 +0800

    update experience and research card

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ module.exports = {
   introduction: [
     // 'My name is Yihao Liu, ...',
     // 'My interests are in ...',
-    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   ],
   avatar: 'avatar.png',
   professions: [
@@ -20,7 +20,8 @@ module.exports = {
   email: 'liuyh970615@gmail.com',
   postsForArchivePage: 3,
   defaultLanguage: 'en',
-  disqusScript: process.env.DISQUS_SCRIPT || 'https://rolwinreevan.disqus.com/embed.js',
+  disqusScript: process.env.DISQUS_SCRIPT
+    || 'https://rolwinreevan.disqus.com/embed.js',
   pages: {
     home: '/',
     posts: 'posts',
@@ -43,47 +44,93 @@ module.exports = {
     editor: 'd851639a-28d8-4884-949f-d338a858f7e9',
     os: 'caf7d0d1-8fd2-4595-a991-363c8583fea9',
   },
-  contactFormUrl: process.env.CONTACT_FORM_ENDPOINT || 'https://getform.io/f/09a3066f-c638-40db-ad59-05e4ed71e451',
+  contactFormUrl: process.env.CONTACT_FORM_ENDPOINT
+    || 'https://getform.io/f/09a3066f-c638-40db-ad59-05e4ed71e451',
   googleAnalyticTrackingId: process.env.GA_TRACKING_ID || '',
-  education: [{
-    date: 'Jan 2020 - Present',
-    icon: 'graduation-cap',
-    title: 'Ph.D in Computer Science',
-    location: 'Nanyang Technological University, Singapore',
-  }, {
-    date: 'Sept 2015 - Aug 2019',
-    icon: 'university',
-    title: 'B.S.E. in Electrical and Computer Engineering',
-    location: 'Shanghai Jiao Tong University, China',
-  }, {
-    date: 'Sept 2008 - June 2015',
-    icon: 'school',
-    title: 'Middle School',
-    location: 'Shanghai Southwest Weiyu Middle School, China',
-  }],
-  interests: [{
-    icon: 'cubes',
-    title: 'Distributed Systems',
-  }, {
-    icon: 'layer-group',
-    title: 'Full Stack Development',
-  }, {
-    icon: ['fab', 'linux'],
-    title: 'Open Source Community',
-  }],
-  experience: [{
-    date: 'Aug 2019',
-    title: 'fxh home visit third time',
-    location: 'Shanghai',
-  }, {
-    date: 'Aug 2018',
-    title: 'fxh home visit second time',
-    location: 'Shanghai',
-  }, {
-    date: 'Aug 2017',
-    title: 'fxh home visit',
-    location: 'Shanghai',
-  }],
+  education: [
+    {
+      date: 'Jan 2020 - Present',
+      icon: 'graduation-cap',
+      title: 'Ph.D in Computer Science',
+      location: 'Nanyang Technological University, Singapore',
+    }, {
+      date: 'Sept 2015 - Aug 2019',
+      icon: 'university',
+      title: 'B.S.E. in Electrical and Computer Engineering',
+      location: 'Shanghai Jiao Tong University, China',
+    }, {
+      date: 'Sept 2008 - June 2015',
+      icon: 'school',
+      title: 'Middle School',
+      location: 'Shanghai Southwest Weiyu Middle School, China',
+    }],
+  interests: [
+    {
+      icon: 'cubes',
+      title: 'Distributed Systems',
+    }, {
+      icon: 'layer-group',
+      title: 'Full Stack Development',
+    }, {
+      icon: ['fab', 'linux'],
+      title: 'Open Source Community',
+    }],
+  experience: [
+    {
+      title: 'Work',
+      data: [
+        {
+          date: 'Aug 2019',
+          title: 'fxh home visit third time',
+          location: 'Shanghai',
+          description: 'description',
+          icon: 'cubes',
+        },
+      ],
+    }, {
+      title: 'Teaching',
+      data: [
+        {
+          date: 'Aug 2019',
+          title: 'fxh home visit third time',
+          location: 'Shanghai',
+        }, {
+          date: 'Aug 2018',
+          title: 'fxh home visit second time',
+          location: 'Shanghai',
+        }, {
+          date: 'Aug 2017',
+          title: 'fxh home visit',
+          location: 'Shanghai',
+        }, {
+          date: 'Aug 2018',
+          title: 'fxh home visit second time',
+          location: 'Shanghai',
+        }, {
+          date: 'Aug 2017',
+          title: 'fxh home visit',
+          location: 'Shanghai',
+        },
+      ],
+    }, {
+      title: 'Volunteer',
+      data: [
+        {
+          date: 'Aug 2019',
+          title: 'fxh home visit third time',
+          location: 'Shanghai',
+        }, {
+          date: 'Aug 2018',
+          title: 'fxh home visit second time',
+          location: 'Shanghai',
+        }, {
+          date: 'Aug 2017',
+          title: 'fxh home visit',
+          location: 'Shanghai',
+        },
+      ],
+    },
+  ],
   tagColors: [
     'magenta', 'red', 'volcano', 'orange', 'gold',
     'lime', 'green', 'cyan', 'blue', 'geekblue', 'purple',
